398ed8247b1c1ba9f94cf06e49d4bc2ad3cc1b13
lkar0014 <lkar0014@student.monash.edu>
2026-08-03 13:19:10 -0400

    Footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer className="footer">
+
+      <p>
+        © {new Date().getFullYear()} Kavindi Karunaratne
+      </p>
+
+      <p>
+        Built with React + Vite
+      </p>
+
+    </footer>
+  );
+}
+
+export default Footer;
